fd6348ab9a445b8acaa728958b101dbfe0af873b
Shreyas Agarwal <shreyasagarwal29@gmail.com>
2026-06-20 11:59:12 +0000

    folder path resolution correction for producton

--- packages/gtfs_s/scripts/transit_subset/paths.py
+++ packages/gtfs_s/scripts/transit_subset/paths.py
@@ -2,8 +2,10 @@ from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[3]
 
-RAW_DIR = PROJECT_ROOT/ "data" / "raw"
-PROCESSED_DIR = PROJECT_ROOT/ "data" / "processed"
+GTFS_S_DIR = PROJECT_ROOT / "gtfs_s"
+
+RAW_DIR = GTFS_S_DIR / "raw"
+PROCESSED_DIR = GTFS_S_DIR / "processed"
 
 GTFS_DIR = sorted(
     RAW_DIR.glob("gtfs_fp*"),
